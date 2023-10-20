e4c0b677bbeacdd2a7d6643e080d78fe0370c6e8
Zhaowenb <106760443+Zhaowenb@users.noreply.github.com>
2023-10-20 12:08:05 +0000

    good

--- airline_reservation_system/build/CMakeFiles/Makefile.cmake
+++ airline_reservation_system/build/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main2.dir/DependInfo.cmake"
+  "CMakeFiles/main.dir/DependInfo.cmake"
   )
